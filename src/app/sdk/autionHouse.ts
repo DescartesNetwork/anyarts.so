7717ccd9a473cc92a4a6cd658e11ad932849c1af
tuphan-dn <tuphan@descartes.network>
2022-05-16 09:31:35 +0700

    _

--- src/app/sdk/autionHouse.ts
+++ src/app/sdk/autionHouse.ts
@@ -1,3 +1,9 @@
+/**
+ * References
+ * - https://docs.rs/mpl-auction-house/latest/mpl_auction_house/
+ * - https://www.npmjs.com/package/@metaplex-foundation/mpl-auction-house
+ */
+
 import { PublicKey } from '@solana/web3.js'
 import { Net } from 'shared/runtime'
 
@@ -8,7 +14,7 @@ class AuctionHouseSDK {
   }
 
   static programId = new PublicKey(
-    'M2mx93ekt1fmXSVkTrUL9xVFHkmME8HTUi5Cyc5aF7K',
+    'hausS13jsjafwWwGqZTUQRmWyvyxn9EQpqMwV1PBBmk',
   )
 }
 
